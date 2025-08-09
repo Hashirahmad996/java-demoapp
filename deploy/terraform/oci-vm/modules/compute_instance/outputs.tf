2cff4c75dce869954ff15f29c953531b48395440
Syed Hashir Ahmad <66515282+Hashirahmad996@users.noreply.github.com>
2025-08-09 14:23:14 +0500

    Update outputs.tf
    update

--- deploy/terraform/oci-vm/modules/compute_instance/outputs.tf
+++ deploy/terraform/oci-vm/modules/compute_instance/outputs.tf
@@ -1,4 +1,13 @@
-output "public_ip" {
-  value = oci_core_instance.vm.public_ip
+# Get the first (primary) VNIC
+data "oci_core_vnic_attachments" "vm_vnics" {
+  compartment_id      = var.compartment_ocid
+  instance_id         = oci_core_instance.vm.id
 }
 
+data "oci_core_vnic" "primary_vnic" {
+  vnic_id = data.oci_core_vnic_attachments.vm_vnics.vnic_attachments[0].vnic_id
+}
+
+output "vm_public_ip" {
+  value = data.oci_core_vnic.primary_vnic.public_ip_address
+}
